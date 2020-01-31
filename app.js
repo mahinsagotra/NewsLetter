b536910906ba01c420dc9205809e42bfa137e79c
Mahin Sagotra <mahinsagotra18@gmail.com>
2020-01-31 15:02:14 +0530

    final changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const request = require("request");
 
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
